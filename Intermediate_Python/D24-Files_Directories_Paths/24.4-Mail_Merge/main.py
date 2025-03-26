d7404575a2d9e29533254e3cf2648a24d3604524
coderBri <coderbri23@gmail.com>
2025-03-25 21:26:31 -0400

    WIP project 24.
    Setting up project and working through solution.

---
+++ Intermediate_Python/D24-Files_Directories_Paths/24.4-Mail_Merge/main.py
@@ -0,0 +1,7 @@
+# TODO: Create a letter using starting_letter.txt
+# * 1. For each name in invited_names.txt, replace the [name] placeholder with the actual name.
+# * 2. Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
